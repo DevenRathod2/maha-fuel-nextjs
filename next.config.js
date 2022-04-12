3c9f46a0651011a405c324fe05e746adf0596927
Deven Rathod <devenrathod2001@gmail.com>
2022-04-12 16:04:00 +0530

    :zap: Initial commit

--- next.config.js
+++ next.config.js
@@ -4,3 +4,14 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '/',
+          destination: 'https://fule-api-india.herokuapp.com/:path*',
+        },
+      ]
+    },
+};
